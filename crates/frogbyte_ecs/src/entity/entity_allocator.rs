3670943712b77d15bcbf5c1f81ee26b4ac53a9b8
Jordan GRILLY <jordangrilly@gmail.com>
2026-08-08 12:01:46 +0200

    feature: implement default for EntityAllocator

--- crates/frogbyte_ecs/src/entity/entity_allocator.rs
+++ crates/frogbyte_ecs/src/entity/entity_allocator.rs
@@ -11,6 +11,12 @@ pub struct EntityAllocator {
     free: Vec<u32>,
 }
 
+impl Default for EntityAllocator {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl EntityAllocator {
     pub fn new() -> Self {
         Self {
